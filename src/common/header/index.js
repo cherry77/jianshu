8b762bf86e9128c9c53dc89bccc10be1931cafdc
flora <15071285862@163.com>
2019-11-17 11:37:41 +0800

    use immutable/react-immutable to update state which can't be allow update state directly

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -43,7 +43,8 @@ const Header = (props) => {
 }
 const mapStateToProps = (state) => {
     return {
-        focused: state.header.focused
+        // focused: state.get('header').get('focused')
+        focused: state.getIn(['header', 'focused'])
     }
 }
 const mapDispatchToProps = (dispatch) => {
